c4e0434f8b8a607c8317988ec396364cb77c8c99
jinpeng wang <jason.wang@aelf.io>
2022-12-24 14:38:22 +0800

    Change graphQL for block indexer demo

--- examples/GraphQL.Client.Example/Program.cs
+++ examples/GraphQL.Client.Example/Program.cs
@@ -11,36 +11,35 @@ public static class Program
 {
     public static async Task Main()
     {
-        using var graphQLClient = new GraphQLHttpClient("https://swapi.apis.guru/", new NewtonsoftJsonSerializer());
+        using var graphQLClient = new GraphQLHttpClient("http://localhost:5000/AElfIndexer_DApp/testschema/graphql", new NewtonsoftJsonSerializer());
 
-        var personAndFilmsRequest = new GraphQLRequest
+        var testBlockRequest = new GraphQLRequest
         {
             Query = @"
-			    query PersonAndFilms($id: ID) {
-			        person(id: $id) {
-			            name
-			            filmConnection {
-			                films {
-			                    title
-			                }
-			            }
-			        }
-			    }",
-            OperationName = "PersonAndFilms",
+			    query ($id: String!) {
+                    blockTest(id: $id){
+                        chainId
+                        blockHash,
+                        previousBlockHash,
+                        blockHeight,
+                        blockTime,
+                        signature,
+                        confirmed
+                    }
+                }",
+            // OperationName = "PersonAndFilms",
             Variables = new
             {
-                id = "cGVvcGxlOjE="
+                id = "99a7c5f26b104fc3c3d7ab9decd30cb813c43f8113063c1b7f3a3b849f3487eb"
             }
         };
 
-        var graphQLResponse = await graphQLClient.SendQueryAsync<PersonAndFilmsResponse>(personAndFilmsRequest);
+        var graphQLResponse = await graphQLClient.SendQueryAsync<TestBlockResponse>(testBlockRequest);
         Console.WriteLine("raw response:");
         Console.WriteLine(JsonSerializer.Serialize(graphQLResponse, new JsonSerializerOptions { WriteIndented = true }));
 
         Console.WriteLine();
-        Console.WriteLine($"Name: {graphQLResponse.Data.Person.Name}");
-        var films = string.Join(", ", graphQLResponse.Data.Person.FilmConnection.Films.Select(f => f.Title));
-        Console.WriteLine($"Films: {films}");
+        Console.WriteLine($"Name: {graphQLResponse.Data.BlockTest.Id}");
 
         Console.WriteLine();
         Console.WriteLine("Press any key to quit...");
